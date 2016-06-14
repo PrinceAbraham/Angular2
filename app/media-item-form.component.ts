42c5beeb2f9a4734e9578d0c0b00a61bc46a4dfc
hunterabraham <prince@Princes-iMac.local>
2016-06-14 10:58:45 -0400

    Model Driven Form
    The model-driven approach is another way to handle building forms in
    Angular. It has a lot of similarities to the template-driven approach,
    but the big difference is the form is built in the component class.
    That allows you to have a contract that your form needs to adhere to,
    the ability to set up validation rules, subscribe to field value
    changes, and ultimately unit test your form logic, without any UI
    layer. So template-driven gives you an easy way to get up and running,
    whereas model-driven allows you to take your forms wherever they need
    to go.

--- app/media-item-form.component.ts
+++ app/media-item-form.component.ts
@@ -1,11 +1,28 @@
 import {Component} from 'angular2/core';
 
+//Model Driven Form uses control and control group
+import {ControlGroup, Control} from 'angular2/common';
+
 @Component({
     selector: 'media-item-form',
     templateUrl: 'app/media-item-form.component.html',
     styleUrls: ['app/media-item-form.component.css']
 })
 export class MediaItemFormComponent {
+    //decalare a form
+    form;
+
+    //call initialize during the lifecycle
+    ngOnInit(){
+        this.form = new ControlGroup({
+            //Give properties to the new ControlGroup
+            'medium': new Control('Movies'), //Setting default to Movies
+            'name' : new Control(''),
+            'category' : new Control(''),
+            'year' : new Control('')
+        });
+    }
+
     onSubmit(mediaItem){
         console.log(mediaItem);
     }
