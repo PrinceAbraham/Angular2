36fb7a829097abbe21fd17fcc5314c62594833cd
hunterabraham <prince@Princes-iMac.local>
2016-06-15 15:39:01 -0400

    Creating Mock-xhr-backend and replacing the default xhr with it

--- app/main.ts
+++ app/main.ts
@@ -5,9 +5,13 @@ import {MediaItemService} from './media-item.service';
 //provider
 import {provide} from 'angular2/core';
 import {LOOKUP_LISTS, lookupLists} from './providers';
+import {HTTP_PROVIDERS, XHRBackend} from 'angular2/http';
+import {MockXHRBackend} from './mock-xhr-backend';
 
 
 bootstrap(AppComponent, [
     MediaItemService,
-    provide(LOOKUP_LISTS,{useValue : lookupLists})
-    ]);
+    provide(LOOKUP_LISTS, { useValue: lookupLists }),
+    HTTP_PROVIDERS,
+    provide(XHRBackend, {useClass: MockXHRBackend})
+]);
